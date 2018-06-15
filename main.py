78dc7fd235c142aea52bd7af1e11893aa01b5305
William Zhang <willzhang05@gmail.com>
2018-06-15 11:49:01 -0400

    main functionality working

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from lxml import etree
 
 
 def main():
-    hostname = input("Device hostname: ")
-    username = input("Device username: ")
+    print("hi")
+    hostname = "vsrx2" #input("Device hostname: ")
+    username = "root" #input("Device username: ")
     password = getpass("Device password: ")
     dev = Device(host=hostname, user=username, passwd=password)
     try:
@@ -21,9 +22,22 @@ def main():
         print (err)
         sys.exit(1)
     #output = dev.display_xml_rpc('show isis adjacency')
-    output = dev.rpc.get_config()
-    print(etree.tostring(output, encoding='unicode'))
+    output = dev.rpc.get_route_information()
+    find = etree.XPath('//rt')
+    matches = find(output)
+    routes = []
+    for m in matches:
+        entry = dict()
+        type_find = etree.XPath('rt-entry//protocol-name')
+        entry['type'] = type_find(m)[0].text
+        dest_find = etree.XPath('rt-destination')
+        entry['dest'] = dest_find(m)[0].text
+        int_find = etree.XPath('rt-entry//nh/via')
+        int_res = int_find(m)
+        entry['int'] = int_find(m)[0].text if len(int_res) > 0 else None
+        routes.append(entry)
 
+    print(routes)
     dev.close()
 
 if __name__ == '__main__':
